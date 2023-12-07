2cea86f2f1705f530b6827415037f3aa979ad923
DmitryGP <dimgeo@yandex.ru>
2023-12-07 16:47:01 +0300

    Д.з. #1. Xml-конфигурация.

--- hw01/src/main/java/org/dgp/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/org/dgp/hw/dao/CsvQuestionDao.java
@@ -1,9 +1,15 @@
 package org.dgp.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.dgp.hw.config.TestFileNameProvider;
+import org.dgp.hw.dao.dto.QuestionDto;
 import org.dgp.hw.domain.Question;
+import org.dgp.hw.exceptions.QuestionReadException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +19,35 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        String questionCsvFileName = fileNameProvider.getTestFileName();
+        List dtoList;
+
+        try {
+            Reader streamReader = getStreamReaderFor(questionCsvFileName);
+
+            dtoList = new CsvToBeanBuilder(streamReader)
+                    .withType(QuestionDto.class).withSeparator(';').withSkipLines(1).build().parse();
+
+        } catch (Exception exc) {
+            throw new QuestionReadException("Error while reading questions", exc);
+        }
+
+        List<Question> questions = new ArrayList<>();
+
+        for (Object o :
+                dtoList) {
+            QuestionDto qDto = (QuestionDto) o;
+            questions.add(qDto.toDomainObject());
+        }
+
+        return questions;
+    }
+
+    private Reader getStreamReaderFor(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        return new InputStreamReader(inputStream);
     }
 }
--- hw01/src/main/java/org/dgp/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/org/dgp/hw/service/TestServiceImpl.java
@@ -2,15 +2,35 @@ package org.dgp.hw.service;
 
 import lombok.RequiredArgsConstructor;
 
+import org.dgp.hw.dao.QuestionDao;
+import org.dgp.hw.domain.Answer;
+import org.dgp.hw.domain.Question;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao questionDao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        List<Question> questions = questionDao.findAll();
+
+        for (Question q :
+                questions) {
+            ioService.printFormattedLine("%s", q.text());
+
+            ioService.printFormattedLine("\tPossible answers:");
+            for (Answer answer :
+                    q.answers()) {
+                ioService.printFormattedLine("\t\t%s", answer.text());
+            }
+        }
     }
 }
