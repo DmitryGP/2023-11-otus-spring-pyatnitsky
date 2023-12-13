c8c2195ccb1359967d94537c6d8c28df550c4e58
DmitryGP <dimgeo@yandex.ru>
2023-12-13 19:26:57 +0300

    Д.з. - Annotation configuration

--- hw02/src/main/java/org/dgp/hw/Application.java
+++ hw02/src/main/java/org/dgp/hw/Application.java
@@ -2,16 +2,16 @@ package org.dgp.hw;
 
 import org.springframework.context.ApplicationContext;
 import org.dgp.hw.service.TestRunnerService;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "org.dgp")
 @PropertySource("application.properties")
 public class Application {
     public static void main(String[] args) {
-
-        //Прописать бины в spring-context.xml и создать контекст на его основе
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
