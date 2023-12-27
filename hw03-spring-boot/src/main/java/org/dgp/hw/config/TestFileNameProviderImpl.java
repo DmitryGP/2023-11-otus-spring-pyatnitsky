aee124585a82aac03188be5c48201bbdf78249eb
DmitryGP <dimgeo@yandex.ru>
2023-12-27 18:17:41 +0300

    hw03 - Spring boot.

--- hw03-spring-boot/src/main/java/org/dgp/hw/config/TestFileNameProviderImpl.java
+++ hw03-spring-boot/src/main/java/org/dgp/hw/config/TestFileNameProviderImpl.java
@@ -1,22 +1,17 @@
 package org.dgp.hw.config;
 
-import org.springframework.context.MessageSource;
+import lombok.AllArgsConstructor;
+import org.dgp.hw.service.LocalizedStringService;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class TestFileNameProviderImpl implements TestFileNameProvider {
 
-    private final AppProps appProps;
-
-    private final MessageSource messageSource;
-
-    public TestFileNameProviderImpl(AppProps appProps, MessageSource messageSource) {
-        this.appProps = appProps;
-        this.messageSource = messageSource;
-    }
+    private final LocalizedStringService localizedStringService;
 
     @Override
     public String getTestFileName() {
-        return messageSource.getMessage("questionFile", null, appProps.getLocale());
+        return localizedStringService.getString("questionFile");
     }
 }
